5097fca97228d9881edfbc25ff940a4ab3287a58
ligggooo <ligggooowow@163.com>
2018-11-03 01:26:45 +0800

    Renamed directories.

---
+++ mod1_homework_3/cart.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@Author  :   Goodwillie
+@Software:   PyCharm
+@File    :   cart.py
+@Time    :   2018/11/2 23:57
+@Desc    :   Luffycity homework 3
+'''
+# 作业题目：购物车程序
+# 功能要求：
+# 1、启动程序后，输入用户名密码后，让用户输入工资，然后打印商品列表
+# 2、允许用户根据商品编号购买商品
+# 3、用户选择商品后，检测余额是否够，够就直接扣款，不够就提醒
+# 4、可随时退出，退出时，打印已购买商品和余额
+# 5、在用户使用过程中， 关键输出，如余额，商品已加入购物车等消息，需高亮显示
+# 6、用户下一次登录后，输入用户名密码，直接回到上次的状态，即上次消费的余额什么的还是那些，再次登录可继续购买
+# 7、允许查询之前的消费记录
+from utils import load_record,save_record,user_auth,show_goods,buy_goods,print_log
+
+goods = [
+{"name": "电脑", "price": 1999},
+{"name": "鼠标", "price": 10},
+{"name": "游艇", "price": 20},
+{"name": "美女", "price": 998},
+]
+
+status_file = 'status.dat'  # 文件中写入1，则登录可用，否则登录不可用
+record_file = './record.dat' # 用于记录余额,若某个用户有消费记录，则程序不会提示输入工资
+users = [['luffy', '12345'], ['zoro', '12341'], ['chopper', '321'], ['robin', 'absd']]  # 账户名和密码
+
+
+record = load_record(record_file)
+user = user_auth(users,status_file)
+if user:
+    if user not in record:
+        money = float(input('无消费记录，请输入你的工资：'))
+    else:
+        money = record[user]
+        print('查到消费记录，你的余额为%s'%money)
+    shopping_log = {}
+    while 1:
+        print('商品')
+        show_goods(goods)
+        select = input('您当前的余额为%s,输入商品序号购买，或者q退出：'%money)
+        if select == 'q':
+            print_log(shopping_log,money)
+            save_record(record_file,record,user,money)
+            exit('退出')
+        elif select.isdigit() and int(select) >=0 and int(select) <len(goods):
+            money = buy_goods(goods,int(select),money,shopping_log)
+        else:
+            print('无效输入')
+
+
+
+
---
+++ mod1_homework_3/utils.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@Author  :   Goodwillie
+@Software:   PyCharm
+@File    :   utils.py
+@Time    :   2018/11/3 0:14
+@Desc    :
+'''
+
+
+def user_auth(users,status_file):
+    status = open(status_file).read()
+    if status == '1':
+        try_left = 3
+        while try_left > 0:
+            user_exist = False
+            user_name = input('请输入用户名： ')
+            user_passwd = input('请输入密码： ')
+            for user_recored in users:
+                user_name_recored = user_recored[0]
+                user_passwd_recored = user_recored[1]
+                if user_name == user_name_recored:
+                    user_exist = True
+                    if user_passwd == user_passwd_recored:
+                        print('Welcome,', user_name, '!')
+                        return user_name
+                    break
+            try_left -= 1
+            if not user_exist:
+                print('无此账户,还有%s次机会' % try_left)
+            else:
+                print('密码错误,还有%s次机会' % try_left)
+        print('三次错误，锁定')
+        open(status_file, 'w').write('0')
+        return 0
+    else:
+        print('登录锁定，无法使用')
+        return 0
+
+def show_goods(goods):
+    for i,item in enumerate(goods):
+        print('%s. %s, price:%s'%(i,item['name'],item['price']))
+
+def buy_goods(goods,select,money,shopping_log):
+    item = goods[select]
+    price = item['price']
+    if money >= price:
+        money -= price
+        print("\033[1;31;40m您购买了%s, 余额为%s.\033[0m"%(item['name'],money))
+        if item['name'] in shopping_log:
+            shopping_log[item['name'] ]+=1
+        else:
+            shopping_log[item['name']] = 1
+    else:
+        print("\033[1;31;40m余额不足，购买失败！\033[0m")
+    return money
+
+def print_log(log,money):
+    print('购物记录:')
+    if len(log)==0:
+        print('未购买任何物品，余额为%s'%money)
+    else:
+        for item in log:
+            print('你购买了%s单位%s'%(log[item],item))
+        print('你的余额为%s' % money)
+
+def load_record(record_file):
+    record = {}
+    for line in open(record_file).readlines():
+        name,money = line.strip().split('\t')
+        record[name] = float(money)
+    return record
+
+def save_record(record_file,record,user,money):
+    record[user] = money
+    f= open(record_file,'w')
+    for user in record:
+        line = user+'\t'+str(record[user])+'\n'
+        f.write(line)
+    f.close()
