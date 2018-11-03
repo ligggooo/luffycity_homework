d2c906ab590b270ab4d0b36d91d8d75b5fd7261a
ligggooo <ligggooowow@163.com>
2018-11-03 21:36:29 +0800

    购物车

---
+++ mod1_homework_1/utils.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@Author  :   Goodwillie
+@Software:   PyCharm
+@File    :   utils.py
+@Time    :   2018/11/3 20:44
+@Desc    :
+'''
+import json
+
+def load_status(file):
+    pass
+
+def write_status(file):
+    pass
